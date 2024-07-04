b9024897b09e83ee74c5433e077f66690b0e729d
KimInteger <kjs20151240@gmail.com>
2024-07-04 11:04:12 +0900

    feat_김정수 : litag와 atag 변수할당

--- src/menu.ts
+++ src/menu.ts
@@ -1,3 +1,21 @@
-const a = 'test';
+const root: HTMLElement = document.getElementById('root') as HTMLDivElement;
 
-console.log(a);
+console.log(root);
+
+const anchorTags = (): string => {
+  let result: string = `<a href="#">메뉴</a>`;
+  return result;
+};
+
+const liTags = (children: string): string => {
+  let result: string = `<li>${children}</li>`;
+  return result;
+};
+
+root.innerHTML = `
+<ul>
+  ${liTags(anchorTags())}
+  ${liTags(anchorTags())}
+  ${liTags(anchorTags())}
+</ul>
+`;
