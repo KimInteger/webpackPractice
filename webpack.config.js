e4d9395518c9e98ccd5a4e3f64f6cd6c4891c953
KimInteger <kjs20151240@gmail.com>
2024-07-04 14:16:14 +0900

    chore_김정수 : ts파일 번들링

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
 
 module.exports = {
-  entry: './dist/menu.js',
+  entry: './src/menu.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
 };
