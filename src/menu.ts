6af2c2479d76caf82e1754e1e0c1e550e947eb41
KimInteger <kjs20151240@gmail.com>
2024-07-04 12:08:11 +0900

    feat_김정수 : 클릭할때마다 색상이 변해요!

--- src/menu.ts
+++ src/menu.ts
@@ -13,17 +13,49 @@ const liTags = (children: string): string => {
 };
 
 const basicData: BasicData = {
-  Menu1: '파인애플피자',
-  Menu2: '고추바사삭순살치킨',
-  Menu3: '빅맥버거',
-  Menu4: '참깨라면',
+  Menu1: {
+    name: '파인애플피자',
+    backgroundColor: 'brown',
+  },
+  Menu2: {
+    name: '고추바사삭순살치킨',
+    backgroundColor: 'orange',
+  },
+  Menu3: {
+    name: '빅맥버거',
+    backgroundColor: 'yellow',
+  },
+  Menu4: {
+    name: '김밥',
+    backgroundColor: 'gray',
+  },
+  Menu5: {
+    name: '신라면',
+    backgroundColor: 'red',
+  },
 };
 
 interface BasicData {
-  Menu1: '파인애플피자';
-  Menu2: '고추바사삭순살치킨';
-  Menu3: '빅맥버거';
-  Menu4: '참깨라면';
+  Menu1: {
+    name: '파인애플피자';
+    backgroundColor: 'brown';
+  };
+  Menu2: {
+    name: '고추바사삭순살치킨';
+    backgroundColor: 'orange';
+  };
+  Menu3: {
+    name: '빅맥버거';
+    backgroundColor: 'yellow';
+  };
+  Menu4: {
+    name: '김밥';
+    backgroundColor: 'gray';
+  };
+  Menu5: {
+    name: '신라면';
+    backgroundColor: 'red';
+  };
 }
 
 const totalElement = (object: BasicData): string => {
@@ -31,7 +63,7 @@ const totalElement = (object: BasicData): string => {
   // liTags(anchorTags(`${object.Menu1}`, object.Menu1));
 
   for (let key in object) {
-    let value = object[key as keyof BasicData];
+    let value = object[key as keyof BasicData].name;
     result += liTags(anchorTags(key, value));
   }
 
@@ -39,11 +71,25 @@ const totalElement = (object: BasicData): string => {
 };
 
 root.innerHTML = `
-<ul>
+<ul id="menu">
   ${totalElement(basicData)}
 </ul>
+<main id="main-target"></main>
 `;
 
 //---------------------------------------------------------------------------------
 // * HTML을 대신해서 만든 HTML 작성 코드
 // * React, CSR방식. 위는 렌더링용 코드.
+
+const mainTarget = document.getElementById('main-target') as HTMLElement;
+
+console.log(window.location.hash);
+
+window.addEventListener('hashchange', (): void => {
+  const hash = window.location.hash.substring(1);
+  console.log(hash);
+
+  mainTarget.innerHTML = `<h1>${basicData[hash as keyof BasicData].name}</h1>`;
+  mainTarget.style.backgroundColor =
+    basicData[hash as keyof BasicData].backgroundColor;
+});
